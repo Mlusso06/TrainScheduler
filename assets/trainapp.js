3cb81647677c7d645fabaa567609e726986bea80
spe1901 <52164194+Mlusso06@users.noreply.github.com>
2020-02-09 21:49:26 -0600

    fixed the Html to, moved the trainapp.js to bottom, built new firedata base link and project

--- assets/trainapp.js
+++ assets/trainapp.js
@@ -1,51 +1,78 @@
 // Link to Fire base
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyBvsAJHI7I8JojrFAyRkvFZcaKKKHU5Ab0",
-    authDomain: "trainscheduler-ae4cf.firebaseapp.com",
-    databaseURL: "https://trainscheduler-ae4cf.firebaseio.com",
-    projectId: "trainscheduler-ae4cf",
-    storageBucket: "trainscheduler-ae4cf.appspot.com",
-    messagingSenderId: "278830555255",
-    appId: "1:278830555255:web:1f8b54db2bb47efaade672"
+    apiKey: "AIzaSyB7_Gga8fVbL8XybnjiH5MegsfjPf2AiXA",
+    authDomain: "trainschd2.firebaseapp.com",
+    databaseURL: "https://trainschd2.firebaseio.com",
+    projectId: "trainschd2",
+    storageBucket: "trainschd2.appspot.com",
+    messagingSenderId: "350192476547",
+    appId: "1:350192476547:web:4ee9267a57ded7f23a56b6"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 // basic variable to ref items in the firebase data base
-var trainInfo = firebase.database();
+var database = firebase.database();
 // need an on click listener so when you add your info, it will be sent to the firebase data base
-$("#button1").on("click", function () {
+$("#button1").on("click", function (event) {
+    event.preventDefault()
+    // looking at activity 17 from Firebase week - employee time sheet
+    var trainNm = $("#train-name-input").val().trim();
+    var destination = $("#destInfo").val().trim();
+    var firstTrainTime = $("#firstTime").val().trim();
+    var frequency = $("#frequencyInfo").val().trim();
 
-    trainInfo = $("#trainName").val().trim();
-    destination = $("#destInfo").val().trim();
-    // using info from Activity 21 to convert the time, and subtrack the start time
-    firstTrainTime = moment($("#firstTime").val().trim(), "HH:mm").subtract(1, "years");
-    frequency = $("frequencyInfo").val().trim();
-    console.log(trainInfo);
+    console.log(trainNm);
     console.log(destination);
     console.log(firstTrainTime);
     console.log(frequency);
-    // capture the info 
-    newTrainInfo = {
-        trainInfo: trainInfo,
+    // capture the info and create "temp" objects for holding the Train info
+    // var newTrainInfo = {
+    //     trainNm: trainNm,
+    //     destination: destination,
+    //     firstTrainTime: firstTrainTime,
+    //     frequency: frequency
+    // };
+    // This is the code to push the data caputured to firebase 
+    database.ref().push({
+        trainNm: trainNm,
         destination: destination,
         firstTrainTime: firstTrainTime,
-        frequency: frequency
-    }
-    // This is the code to push the data caputured to firebase 
-    trainInfo.ref().push(newTrainInfo);
+        frequency: frequency,
 
-    $("#trainName").val("");
+    });
+
+
+
+    console.log(database.trainNm);
+    console.log(database.destination);
+    console.log(database.firstTrainTime);
+    console.log(database.frequency);
+
+    // this will now clear all the text-boxes
+    $("#train-name-input").val("");
     $("#destInfo").val("");
-    $("firstTime").val("");
-    $("frequencyInfo").val("");
+    $("#firstTime").val("");
+    $("#frequencyInfo").val("");
 
-    return false;
+
 
 })
 // // grab the info from the data base -- use the info from activity 19 - add a child
-// trainInfo.ref().on("child_added", function (childSnapshot) {
+database.ref().on("child_added", function (childSnapshot) {
+    console.log(childSnapshot.val());
+
+    var trainNm = childSnapshot.val().trainNm;
+    var destination = childSnapshot.val().destination;
+    var firstTrainTime = childSnapshot.val().firstTrainTime;
+    var frequency = childSnapshot.val().frequency;
+    var remainingTm = moment().diff(moment.unix(firstTrainTime), "minutes") % frequency;
+    var minutes = frequency - remainingTm;
+    var arrTime = moment().add(minutes, "m").format("HH:mm");
+
+
+});
 
 //     // Log everything that's coming out of snapshot
 //     console.log(childSnapshot.val().trainInfo);
